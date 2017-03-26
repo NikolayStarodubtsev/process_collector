d859b28e0c92206bceca7bf1fadae83b2f1ee867
Nikolay Starodubtsev <starodubcevna@gmail.com>
2017-03-26 15:27:18 +0300

    Remove unused pip dependency.
    Remove datetime dependency, since we use standart datetime library
    not the one from PyPi.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    install_requires=['psutil', 'datetime'],
+    install_requires=['psutil'],
 
     entry_points={
         'console_scripts': [
